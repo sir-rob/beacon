570c5edf96a10ed89056976edf798a166562d7f2
neojester22 <alanwong22@gmail.com>
2013-11-17 14:56:31 -0500

    HelloWorld

---
+++ prototypes/Alan/hello/platforms/android/bin/classes.dex.d
@@ -0,0 +1,18 @@
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes.dex : \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/com/example/hello/BuildConfig.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/com/example/hello/HelloWorld.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/com/example/hello/R$attr.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/com/example/hello/R$drawable.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/com/example/hello/R$string.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/com/example/hello/R$xml.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/com/example/hello/R.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/contacts/ContactAccessor$WhereOptions.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/contacts/ContactAccessor.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/contacts/ContactAccessorSdk5.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager$1.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager$2.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager$3.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/device/Device$1.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/classes/org/apache/cordova/device/Device.class \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/dexedLibs/cordova-3.1.0-e27e4628866aa71b09a414c2c88d0ce7.jar \
---
+++ prototypes/Alan/hello/platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/gen/com/example/hello/R.java \
+ : /Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/res/drawable/icon.png \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/res/drawable-hdpi/icon.png \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/res/drawable-ldpi/icon.png \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/res/drawable-mdpi/icon.png \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/res/values/strings.xml \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/res/xml/config.xml \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/AndroidManifest.xml \
