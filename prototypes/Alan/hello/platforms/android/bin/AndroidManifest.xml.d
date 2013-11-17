465ccf9edf475ffe53881940880233bf94482119
neojester22 <alanwong22@gmail.com>
2013-11-17 14:55:59 -0500

    HelloWorld

---
+++ prototypes/Alan/hello/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/bin/AndroidManifest.xml : \
+/Users/Station22/Desktop/workspace/beacon/prototypes/Alan/hello/platforms/android/AndroidManifest.xml \
