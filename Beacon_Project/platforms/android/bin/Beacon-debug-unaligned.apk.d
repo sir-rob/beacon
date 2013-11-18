c86b8325cae972ff61fb9b6c7b1eff10708cfbb1
alanwong22 <alanwong22@gmail.com>
2013-11-17 19:20:10 -0500

    ...

--- Beacon_Project/platforms/android/bin/Beacon-debug-unaligned.apk.d
+++ Beacon_Project/platforms/android/bin/Beacon-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/Beacon-debug-unaligned.apk : \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/Beacon.ap_ \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes.dex \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/libs/cordova-3.1.0.jar \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/Beacon-debug-unaligned.apk : \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/Beacon.ap_ \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes.dex \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/libs/cordova-3.1.0.jar \
--- Beacon_Project/platforms/android/bin/classes.dex.d
+++ Beacon_Project/platforms/android/bin/classes.dex.d
@@ -1,9 +1,18 @@
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes.dex : \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes/com/site/Beacon.class \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes/com/site/BuildConfig.class \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes/com/site/R$attr.class \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes/com/site/R$drawable.class \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes/com/site/R$string.class \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes/com/site/R$xml.class \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/classes/com/site/R.class \
-/Users/Station22/Desktop/workspace/Beacon_Project/Beacon/platforms/android/bin/dexedLibs/cordova-3.1.0-1544782328afa4280de2b580a9ef1438.jar \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes.dex : \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/com/site/Beacon.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/com/site/BuildConfig.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/com/site/R$attr.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/com/site/R$drawable.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/com/site/R$string.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/com/site/R$xml.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/com/site/R.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/contacts/ContactAccessor$WhereOptions.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/contacts/ContactAccessor.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/contacts/ContactAccessorSdk5.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager$1.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager$2.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager$3.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/contacts/ContactManager.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/device/Device$1.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/classes/org/apache/cordova/device/Device.class \
+/Users/Station22/Desktop/workspace/Beacon/Beacon_Project/platforms/android/bin/dexedLibs/cordova-3.1.0-946f42cc860add041120f262a3a9b9c9.jar \
